85744c9ad7cc45510519898196b2bcc459c7b2c1
26brennar <brenna.c.ren@gmail.com>
2025-09-05 15:01:05 -0700

    AB1 Day 1 work; add main method

---
+++ AB1/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Author: Brenna Ren
+ * 
+ * Date of Creation: September 5, 2025
+ * 
+ * Description: INSERT SOMETHING HERE
+ */
+public class Main 
+{
+
+   /*
+    * Main method that contains independent methods that run the Perceptron based on the user's configurations.
+    * First, it sets the configuration parameters and echos them by printing them to the console. Then, it 
+    * allocates memory for the Perceptron and trains/runs it (based on the user's configurations). Afterwards,
+    * it 
+    */
+   public static void main(String[] args) 
+   {
+   
+   }
+}∏
